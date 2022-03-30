0d510daff1e1fea8771d45f063aefedccfbe3da4
Sunil Atri <sunil.atri@gmail.com>
2022-03-29 21:49:27 -0400

    JS changes

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
+// This function will be called when the 'calcuate interest' button is clicked in the HTML file
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-//    if (Number.isInteger(principal) != true)
- //   {
- //       window.alert("Please Enter a valid number");
- //       document.getElementById("principal").focus();
- //       return;
- //   }
-    if (parseFloat(principal) <= 0) {
-        window.alert("Please Enter a positive number");
-        document.getElementById("principal").value = '';
-        document.getElementById("principal").focus();
+    var principal = document.getElementById("principal").value; 
+    //Make sure that something was entered in the box before calculation is allowed
+    if (principal == "") {
+        window.alert("Please Enter a positive number"); // message to user to correct
+        document.getElementById("principal").focus(); // set focus
+        return;
+    }
+    if (parseFloat(principal) <= 0) {    //principal is a string type, convert before check if the user has input a positive number
+        window.alert("Please Enter a positive number"); // message to user to correct
+        document.getElementById("principal").value = ''; // clear the incorrect number
+        document.getElementById("principal").focus(); // set focus
         return;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    // calculate interest, convert interest to float as its a string type
     var interest = years * principal * (parseFloat(rate) / 100);
+    // Date with paremeters defaults to todays date
     var year = new Date().getFullYear() + parseInt(years);
-    var amount = parseFloat(principal) + parseFloat(interest);
+    // construct HTML output string
     document.getElementById("result").innerHTML = "<br /><br />" +
         "If you deposit " + "<mark>" + principal + "</mark>"+ "," + "<br />" +
         "at an interest rate of " + "<mark>" + rate + "</mark>" + "%." + "\<br />" +
-        "you will receive an amount of " + "<mark>" + amount + "</mark>"+ "," + "<br />" +
+        "you will receive an amount of " + "<mark>" + interest + "</mark>"+ "," + "<br />" +
         "in the year " + "<mark>" + year + "</mark>" + "." + "<br />";
-
-
-
 }
-
+// This funciton will be called when the rate slider is moved in the HTML file
+// It sets the number next to the slider to the current value of the slider.
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
-
 }
         
