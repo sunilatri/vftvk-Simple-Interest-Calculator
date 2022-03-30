21eff84da5aa9c49b2a2b564ea36ec4c06ddc7ca
Sunil Atri <sunil.atri@gmail.com>
2022-03-29 21:06:46 -0400

    JS changes

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = years * principal * (parseFloat(rate) / 100);
+    var year = new Date().getFullYear() + parseInt(years);
+    var amount = parseFloat(principal) + parseFloat(interest);
+    document.getElementById("result").innerHTML = "<br /><br />" +
+        "If you deposit " + "<mark>" + principal + "</mark>"+ "," + "<br />" +
+        "at an interest rate of " + "<mark>" + rate + "</mark>" + "%." + "\<br />" +
+        "you will receive an amount of " + "<mark>" + amount + "</mark>"+ "," + "<br />" +
+        "in the year " + "<mark>" + year + "</mark>" + "." + "<br />";
+
+
+
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+
 }
         
